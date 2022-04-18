4209172bf6c769209847595205ffffdb77613e6c
Aditi-247 <71731371+Aditi-247@users.noreply.github.com>
2022-04-18 14:01:48 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -10,7 +10,7 @@ let transporter = nodemailer.createTransport({
     secure:false,
     auth:{
         user:'codeialdevlopment@gmail.com ',
-        pass:'CODEIAL22#'
+        pass:'codeialfinal22'
     }
 });
 
